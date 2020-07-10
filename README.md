bf9a208de3eb973ff2f0c4ed8a4f9302f180d85b
Tomas Bravo <tbravo2019@udec.cl>
2020-07-10 03:53:24 -0400

    Update Test.c and README.md

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # ProyectoProgra1
-El archivo main.c es el codigo que genera un aventana y dubuja la zona de juego eso si solo dibuja
+-El archivo main.c es el codigo que genera una ventana y dibuja la zona de juego eso si solo dibuja
 los cuadradros pero no genera una red.
+
+-El archivo test.c archivo con programa principal tetris con full funcionalidad exceptuando por menu y
+un reinicio automatico del juego 
--- Test.c
+++ Test.c
@@ -6,12 +6,15 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <time.h>
+#include <string.h>
 
 #define TAM 40
 #define WIDTH 12
 #define HEIGHT 17
 SDL_Window *screen = NULL;
 SDL_Renderer *renderer = NULL;
+SDL_Color blanco = {255, 255, 255, 255};
+SDL_Color rojot = {255, 0, 0, 230};
 
 typedef struct {
     SDL_Texture *pos[HEIGHT][WIDTH];
@@ -28,6 +31,13 @@ typedef struct {
     SDL_Texture *images;
 
 } Piezas;
+
+typedef struct {
+    char nombre[30];
+    long long puntaje;
+} Records;
+
+
 Coor Tetraminos[][3] = {
         {{0,  -1}, {0, 1}, {1,  1}}, //L
         {{-1, 0},  {0, 1}, {1,  0}}, //T
@@ -43,7 +53,7 @@ void Init();
 
 void DrawCubes(int x, int y, SDL_Texture *image);
 
-void DrawPlayGround( Tablero *t);
+void DrawPlayGround(Tablero *t);
 
 void DrawFigure(Piezas *pieza);
 
@@ -69,20 +79,41 @@ void EliminarFila(Tablero *t, int fila);
 
 int LineasELiminadas(Tablero *t);
 
-SDL_Texture *GenerarTexto(char *string);
+SDL_Texture *ImprimirTexto(SDL_Texture *aux, SDL_Rect *rect, char *string, SDL_Color *color, int size);
 
-void Actualizar (Tablero *t, Piezas *p, Piezas*next, SDL_Rect rects[], SDL_Texture *texturas[], char *sPunto, char* slineas, int *puntos, int *lineasEliminadas);
+SDL_Texture *ImprimirNumeros(SDL_Texture *aux, SDL_Rect *rect, long long *num, SDL_Color *color, char *string);
 
-void Close(SDL_Texture *textura[], char *spuntos, char  *slineas);
+void
+Actualizar(Tablero *t, Piezas *p, Piezas *next, SDL_Rect rects[], SDL_Texture *texturas[], char *sPunto, char *slineas,
+           long long *puntos, long long *lineasEliminadas);
+
+void LeerRecords(FILE *file, Records *top10);
+
+void GameOver(SDL_Rect *rects, FILE *file, Records *top10, SDL_Event *event, SDL_Texture *texturas[], long long *puntos,
+              char *sPunto);
+
+void ActualizarRecords(FILE *file, Records *top10, Records *new);
+
+void Close(SDL_Texture *textura[], char *spuntos, char *slineas);
 
 int main() {
+    FILE *records;
     srand(time(NULL));
     Init();
     Tablero t;
-    char *sPunto = (char*)malloc(10000000*sizeof(char));
-    int puntos = 0;
-    int lineasEliminadas = 0;
-    char *slineas = (char*)malloc(10000000*sizeof(char));
+    Records *top10 = (Records *) malloc(10 * sizeof(Records));
+    LeerRecords(records, top10);
+    char *sPunto = (char *) malloc(10000000 * sizeof(char));
+    long long puntos = 0;
+    long long lineasEliminadas = 0;
+    char *slineas = (char *) malloc(10000000 * sizeof(char));
+    SDL_Rect rects[] = {{520, 0,   0, 0},    //0
+                        {520, 200, 0, 0},    //1
+                        {520, 400, 0, 0},    //2
+                        {520, 240, 0, 0}, //3
+                        {520, 440, 0, 0}, //4
+                        {TAM * 9,  TAM, 0, 0}};    //5
+
     SDL_Texture *texturas[] = {LoadTexture("assets/VerticalWall.png"),    //0
                                LoadTexture("assets/RightCorner.png"), //1
                                LoadTexture("assets/LeftCorner.png"),  //2
@@ -94,28 +125,25 @@ int main() {
                                LoadTexture("assets/purpleblock.png"), //8
                                LoadTexture("assets/redblock.png"),    //9
                                LoadTexture("assets/yellowblock.png"), //10
-                               GenerarTexto("Proxima Figura"),    //11
-                               GenerarTexto("Puntos"),    //12
-                               GenerarTexto("Lineas Eliminadas"),    //13
+                               ImprimirTexto(texturas[11], &rects[0], "Prixima Figura", &blanco, 32),    //11
+                               ImprimirTexto(texturas[12], &rects[1], "Puntaje", &blanco, 32),    //12
+                               ImprimirTexto(texturas[13], &rects[2], "Lineas Eliminadas", &blanco, 32),    //13
                                NULL,    //14
                                NULL,    //15
-                               NULL};   //16
-   SDL_Rect rects[] = {{520,0,150,40},
-                       {520,200,100,40},
-                       {520,400,180,40},
-                       {520, 240,0, 0},
-                       {520, 440, 0, 0}};
+                               ImprimirTexto(texturas[16], &rects[5], "Game Over", &rojot, 62)};   //16
 
+
+    rects[5].x -= rects[5].w / 2;
     Piezas p, next;
     CrearPieza(&p, texturas[4 + (rand() % 7)]);
-    CrearPieza(&next,texturas[4 + (rand() % 7)]);
+    CrearPieza(&next, texturas[4 + (rand() % 7)]);
     p.central.x = 6;
-    p.central.y = 0;
+    p.central.y = 1;
     LimpiarTablero(&t, texturas);
     SDL_Event event;
     int play = 1, tick = 0, down = 0, auxl;
 
-    Actualizar(&t,&p, &next,rects,texturas,sPunto,slineas,&puntos,&lineasEliminadas);
+    Actualizar(&t, &p, &next, rects, texturas, sPunto, slineas, &puntos, &lineasEliminadas);
     while (play) {
         Piezas aux = p;
         while (SDL_PollEvent(&event) != 0) {
@@ -142,32 +170,45 @@ int main() {
                 }
             }
         }
-        if(tick %20 == 0){
+        if (tick % 20 == 0) {
             p.central.y++;
+            tick = 0;
+            down = 1;
         }
 
         if (HayColision(&t, &p)) {
             p = aux;
+            for (int i = 0; i < 4; ++i) {
+                if (PiezaPos(i, &p).y == 0) {
+                    GameOver(rects, records, top10, &event, texturas, &puntos, sPunto);
+                    play = 0;
+                    break;
+                }
+
+            }
             if (down) {
                 DetensionPieza(&t, &p);
                 auxl = LineasELiminadas(&t);
-                puntos += auxl == 1? auxl*25 : auxl * auxl *100;
+                puntos += auxl == 1 ? auxl * 25 : auxl * auxl * 100;
                 lineasEliminadas += auxl;
                 p = next;
                 CrearPieza(&next, texturas[4 + (rand() % 7)]);
                 p.central.x = 6;
                 p.central.y = 1;
                 down = 0;
-
             }
         }
 
 
-        Actualizar(&t,&p, &next,rects,texturas,sPunto,slineas,&puntos,&lineasEliminadas);
-        SDL_Delay(30);
-        tick++;
+        if (play) {
+            Actualizar(&t, &p, &next, rects, texturas, sPunto, slineas, &puntos, &lineasEliminadas);
+            SDL_Delay(30);
+            tick++;
+        }
+
     }
-    Close(texturas, sPunto,slineas);
+    free(top10);
+    Close(texturas, sPunto, slineas);
     return 0;
 }
 
@@ -297,7 +338,7 @@ int HayColision(Tablero *t, Piezas *p) {
         Coor pos = PiezaPos(i, p);
         if (pos.x < 1 || pos.x >= WIDTH - 1)
             return 1;
-        if (pos.y < 0 || pos.y > HEIGHT - 2)
+        if (pos.y > HEIGHT - 2)
             return 1;
         if (t->pos[pos.y][pos.x] != NULL)
             return 1;
@@ -338,40 +379,141 @@ int LineasELiminadas(Tablero *t) {
     return lineasElim;
 }
 
-SDL_Texture *GenerarTexto(char *string){
-    SDL_Texture *aux;
-    TTF_Font *font = TTF_OpenFont("assets/Montserrat-Regular.ttf", 28);
-    SDL_Color color = {255, 255, 255, 255};
-    SDL_Surface *textsurface = TTF_RenderText_Solid(font, string, color);
+SDL_Texture *ImprimirTexto(SDL_Texture *aux, SDL_Rect *rect, char *string, SDL_Color *color, int size) {
+    TTF_Font *font = TTF_OpenFont("assets/StrickenBrush.ttf", size);
+    SDL_Surface *textsurface = TTF_RenderText_Solid(font, string, *color);
     aux = SDL_CreateTextureFromSurface(renderer, textsurface);
     SDL_FreeSurface(textsurface);
-    return  aux;
-
+    textsurface = NULL;
+    SDL_QueryTexture(aux, NULL, NULL, &rect->w, &rect->h);
+    return aux;
 }
 
-void Actualizar (Tablero *t, Piezas *p, Piezas*next, SDL_Rect rects[], SDL_Texture *texturas[], char *sPunto, char* slineas, int *puntos, int *lineasEliminadas){
-    sprintf(sPunto,"%d",*puntos);
-    texturas[14] = GenerarTexto(sPunto);
-    sprintf(slineas,"%d",*lineasEliminadas);
-    texturas[15] = GenerarTexto(slineas);
-    SDL_QueryTexture(texturas[14],NULL,NULL,&rects[3].w,&rects[3].h);
-    SDL_QueryTexture(texturas[15],NULL,NULL,&rects[4].w,&rects[4].h);
+SDL_Texture *ImprimirNumeros(SDL_Texture *aux, SDL_Rect *rect, long long *num, SDL_Color *color, char *string) {
+    sprintf(string, "%lld", *num);
+    TTF_Font *font = TTF_OpenFont("assets/Montserrat-Regular.ttf", 28);
+    SDL_Surface *textsurface = TTF_RenderText_Solid(font, string, *color);
+    aux = SDL_CreateTextureFromSurface(renderer, textsurface);
+    SDL_FreeSurface(textsurface);
+    textsurface = NULL;
+    SDL_QueryTexture(aux, NULL, NULL, &rect->w, &rect->h);
+    return aux;
+}
+
+void
+Actualizar(Tablero *t, Piezas *p, Piezas *next, SDL_Rect rects[], SDL_Texture *texturas[], char *sPunto, char *slineas,
+           long long *puntos, long long *lineasEliminadas) {
+    texturas[14] = ImprimirNumeros(texturas[14], &rects[3], puntos, &blanco, sPunto);
+    texturas[15] = ImprimirNumeros(texturas[15], &rects[4], lineasEliminadas, &blanco, slineas);
     SDL_RenderClear(renderer);
     DrawPlayGround(t); // ocupa x = 0 , x = WIDTH -1  y = HEIGHT -1
     DrawFigure(p);
     DrawFigure(next);
-    SDL_RenderCopy(renderer, texturas[11], NULL, &rects[0]);
-    SDL_RenderCopy(renderer, texturas[12], NULL, &rects[1]);
-    SDL_RenderCopy(renderer, texturas[13], NULL, &rects[2]);
-    SDL_RenderCopy(renderer,texturas[14],NULL,&rects[3]);
-    SDL_RenderCopy(renderer,texturas[15],NULL,&rects[4]);
+    for (int i = 0; i < 5; ++i) {
+        SDL_RenderCopy(renderer, texturas[i + 11], NULL, &rects[i]);
+    }
     SDL_RenderPresent(renderer);
     SDL_UpdateWindowSurface(screen);
 }
 
-void Close(SDL_Texture *textura[], char *spuntos, char* slineas) {
+void LeerRecords(FILE *file, Records *top10) {
+    file = fopen("assets/record.txt", "r");
+    if (file == NULL)
+        printf("no se puedo abrir los records");
+    int recordAct = 0;
+    while (!feof(file)) {
+        fscanf(file, "%s %lld", top10[recordAct].nombre, &top10[recordAct].puntaje);
+        recordAct++;
+    }
+    fclose(file);
+}
 
-    for (int i = 0; i < 11; ++i) {
+void GameOver(SDL_Rect *rects, FILE *file, Records *top10, SDL_Event *event, SDL_Texture *texturas[], long long *puntos,
+              char *sPunto) {
+    SDL_StartTextInput();
+    Records new;
+    for (int i = 0; i < 30; ++i) {
+        new.nombre[i] = '\0';
+    }
+    texturas[13] = NULL;
+    file = fopen("assets/record.txt","w");
+    texturas[14] = ImprimirNumeros(texturas[14], &rects[3], puntos, &blanco, sPunto);
+    rects[3].x = 12 * TAM;
+    rects[3].y = 6 * TAM - rects[3].h / 2;
+    texturas[12] = ImprimirTexto(texturas[12], &rects[1], "Puntaje Obtenido", &blanco, 30);
+    rects[1].x = 2 * TAM;
+    rects[1].y = 6 * TAM - rects[1].h / 2;
+    texturas[11] = ImprimirTexto(texturas[11], &rects[0], "Ingrese Numbre:", &blanco, 26);
+    rects[0].x = 2 * TAM;
+    rects[0].y = 10 * TAM - rects[0].h / 2;
+    new.puntaje = *puntos;
+    int inputName = 1;
+    while (inputName) {
+        while (SDL_PollEvent(event) != 0) {
+            if (event->type == SDL_QUIT){
+                inputName = 0;
+                break;
+            }
+            else if (event->type == SDL_TEXTINPUT) {
+                strcat(new.nombre, event->text.text);
+                texturas[13] = ImprimirTexto(texturas[13], &rects[2], new.nombre, &blanco, 26);
+                rects[2].x = 12 * TAM;
+                rects[2].y = 10 * TAM - rects[2].h / 2;
+            } else if (event->type == SDL_KEYDOWN)
+                switch (event->key.keysym.sym) {
+                    case SDLK_RSHIFT:
+                        inputName = 0;
+                        break;
+                }
+        }
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texturas[16], NULL, &rects[5]);
+        SDL_RenderCopy(renderer, texturas[12], NULL, &rects[1]);
+        SDL_RenderCopy(renderer, texturas[14], NULL, &rects[3]);
+        SDL_RenderCopy(renderer, texturas[11], NULL, &rects[0]);
+        SDL_RenderCopy(renderer, texturas[13], NULL, &rects[2]);
+        SDL_RenderPresent(renderer);
+        SDL_UpdateWindowSurface(screen);
+    }
+    ActualizarRecords(file,top10,&new);
+    SDL_StopTextInput();
+    SDL_StopTextInput();
+    fclose(file);
+}
+
+void ActualizarRecords(FILE *file, Records *top10, Records *new) {
+
+    if(new->puntaje > top10[0].puntaje)
+        top10[0] = *new;
+    else if(top10[9].puntaje < new->puntaje){
+        top10[9] = *new;
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = i + 1; j < 10; j++)
+            {
+                if (top10[i].puntaje < top10[j].puntaje)
+                {
+                    Records aux = top10[i];
+                    top10[i] = top10[j];
+                    top10[j] = aux;
+                }
+
+            }
+
+        }
+    }
+
+
+
+
+
+    for (int i = 0; i < 10; i++)
+        fprintf(file, "%s %lld\n",top10[i].nombre,top10[i].puntaje);
+}
+
+void Close(SDL_Texture *textura[], char *spuntos, char *slineas) {
+
+    for (int i = 0; i < 17; ++i) {
         SDL_DestroyTexture(textura[i]);
         textura[i] = NULL;
     }
@@ -385,3 +527,8 @@ void Close(SDL_Texture *textura[], char *spuntos, char* slineas) {
     TTF_Quit();
     SDL_Quit();
 }
+//SDL_RenderClear(renderer);
+//SDL_RenderCopy(renderer, textura, NULL, rect);
+//SDL_RenderPresent(renderer);
+//SDL_UpdateWindowSurface(screen);
+//SDL_Delay(5000);
